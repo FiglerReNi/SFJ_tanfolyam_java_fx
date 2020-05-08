08fe8d1f0695246baaec1b512451686b220575bc
FiglerReNi <renatafigler85@gmail.com>
2020-05-08 18:52:29 +0200

    forras

--- Lotto/src/lotto/LottoNezetController.java
+++ Lotto/src/lotto/LottoNezetController.java
@@ -6,15 +6,17 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 public class LottoNezetController implements Initializable {
 
     //final változót nagy betűvel írjuk
+//<editor-fold defaultstate="collapsed" desc="Class Variables">
     private final int MAX = 90;
-    private final int MIN = 1;
-
+    private final int MIN = 1;   
     private int genNum1;
     private int genNum2;
     private int genNum3;
@@ -25,7 +27,17 @@ public class LottoNezetController implements Initializable {
     private int selNum3;
     private int selNum4;
     private int selNum5;
+//</editor-fold>
 
+//<editor-fold defaultstate="collapsed" desc="FXML items">
+    @FXML
+    private Pane basePane;
+    @FXML
+    private Pane alertPane;
+    @FXML
+    private Label alertText;
+    @FXML
+    private Button alertButton;
     @FXML
     private Label label1;
     @FXML
@@ -37,6 +49,8 @@ public class LottoNezetController implements Initializable {
     @FXML
     private Label label5;
     @FXML
+    private Label eredmeny;
+    @FXML
     private TextField input1;
     @FXML
     private TextField input2;
@@ -46,16 +60,20 @@ public class LottoNezetController implements Initializable {
     private TextField input4;
     @FXML
     private TextField input5;
+//</editor-fold>
 
     @FXML
+    private void handleAlertButton(ActionEvent event){
+         basePane.setDisable(false);
+         basePane.setOpacity(1);
+         alertPane.setVisible(false);
+         alertText.setText("");
+    }
+    
+    @FXML
     private void handleButtonAction(ActionEvent event) {
-        //begyűjtjük a beírt számokat
-        selNum1 = Integer.parseInt(input1.getText());
-        selNum2 = Integer.parseInt(input2.getText());
-        selNum3 = Integer.parseInt(input3.getText());
-        selNum4 = Integer.parseInt(input4.getText());
-        selNum5 = Integer.parseInt(input5.getText());
-        //lenullázzuk az előző sorsolásból maradt értékeket
+       
+       //lenullázzuk az előző sorsolásból maradt értékeket
         genNum1 = 0;
         genNum2 = 0;
         genNum3 = 0;
@@ -73,8 +91,35 @@ public class LottoNezetController implements Initializable {
         label3.setText(String.valueOf(genNum3));
         label4.setText(String.valueOf(genNum4));
         label5.setText(String.valueOf(genNum5));
+        
+        calculate();
     }
 
+    private String calculate(){
+        //begyűjtjük a beírt számokat
+        try{
+        selNum1 = Integer.parseInt(input1.getText());
+        selNum2 = Integer.parseInt(input2.getText());
+        selNum3 = Integer.parseInt(input3.getText());
+        selNum4 = Integer.parseInt(input4.getText());
+        selNum5 = Integer.parseInt(input5.getText());
+        }catch(Exception e){
+            basePane.setDisable(true);
+            basePane.setOpacity(0.3);
+            alertPane.setVisible(true);
+            alertText.setText("Nem jó számot adtál meg!");
+            System.out.println(e);
+            return "";
+            
+        } 
+        if(selNum1 < 1 || selNum1 > 90 || selNum2 < 1 || selNum2 > 90 ||selNum3 < 1 || selNum3 > 90 ||selNum4 < 1 || selNum4 > 90 ||selNum5 < 1 || selNum5 > 90  ){
+            basePane.setDisable(true);
+            basePane.setOpacity(0.3);
+            alertPane.setVisible(true);
+            alertText.setText("Minden számnak 1 és 90 között kell lennie");
+        }
+        return "";
+    }
     /*Rekurzív függyvény - Olyan függvény ami saját magát hívja meg, a mi esetünkben egy feltételhez kötötten*/
     private int getRandomNumber() {
         //1-90 ig generál A módszer
